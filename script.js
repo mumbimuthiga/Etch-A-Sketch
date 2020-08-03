6ccc5c556022285c4667b8105e0fb65d0880f0a9
mumbi <veronicmuthiga@gmail.com>
2020-08-03 08:38:32 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var container=document.querySelector('#container');
+var div=document.createElement("div");
+div.style.width="";
+div.style.height="";
+div.style.color="";
+div.style.background="";
+div.innerHTML="mumbi";
+div.classList.add('div');
+//container.appendChild();
+
